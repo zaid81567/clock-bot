930b7a1a9e86fd5988e2fa184bc838e7ece8ac17
Md Zaid <92244182+zaid81567@users.noreply.github.com>
2023-03-06 00:51:31 +0530

    Update clock_ticks_bot.py

--- clock_ticks_bot.py
+++ clock_ticks_bot.py
@@ -1,6 +1,6 @@
 # installed pyTelegramBotAPI
 import telebot
-import time
+# import time
 
 KEY_API = '5961703408:AAHLaZKvHtZ3RBRt51fRaGf-Y-mYWsKTJhw'
 # creating a bot
@@ -11,14 +11,14 @@ bot = telebot.TeleBot(KEY_API)
 def greeting(message):
     bot.send_message(message.chat.id,'Hey there! Welcome to Clock Ticks.')
 
-@bot.message_handler(commands=['time'])
-def time_is(message):
-    seconds_passed_today = (time.time() + 5.5*3600)%(60*60*24)
-    HH = seconds_passed_today//(3600)
-    MM = (seconds_passed_today - HH*3600)//60
-    SS = (seconds_passed_today - (HH*3600 + MM*60))
-    time_now = f'Time >> {int(HH)} : {int(MM)} : {int(SS)}'
-    bot.send_message(message.chat.id , time_now)
+# @bot.message_handler(commands=['time'])
+# def time_is(message):
+#     seconds_passed_today = (time.time() + 5.5*3600)%(60*60*24)
+#     HH = seconds_passed_today//(3600)
+#     MM = (seconds_passed_today - HH*3600)//60
+#     SS = (seconds_passed_today - (HH*3600 + MM*60))
+#     time_now = f'Time >> {int(HH)} : {int(MM)} : {int(SS)}'
+#     bot.send_message(message.chat.id , time_now)
 
 bot.polling()
 
